b35b7beef6c5eb0909aa4251787df13bc76ed25f
carolcavallaro <carol.cavallaro@gmail.com>
2026-08-05 17:24:46 -0300

    Adiciona a resolução do último exercício proposto.

--- 07logisticaEventos.py
+++ 07logisticaEventos.py
@@ -16,3 +16,47 @@ Remoção de Data Reservada: Uma das datas foi confirmada e fechada. Use o méto
 - Ordene essa lista de custos em ordem crescente usando o método .sort() (ou a função sorted()) e exiba os valores ordenados na tela.
 
 """
+
+import math
+
+print('Olá! Bem vindo (a) ao agendamento de Vans Python!')
+
+datasDisponiveis = ["10/10", "15/10", "20/10", "25/10"]
+datasDisponiveis.append("12/10") # como solicitado no exercício
+datasDisponiveis.insert(0, "05/10") # como solicitado no exercício
+
+print(f'Datas disponíveis para agendamento: {datasDisponiveis}')
+dataEscolhida = input('Que data você quer reservar para a conferência? ').strip()
+
+if dataEscolhida in datasDisponiveis:
+    datasDisponiveis.remove(dataEscolhida) # remove a data escolhida da lista de datas disponíveis
+    dataConfirmada = dataEscolhida
+else:
+    dataConfirmada = datasDisponiveis.pop() # conforme solicitado no exercício
+
+print(f'Data {dataConfirmada} confirmada com sucesso!\nDatas disponíveis: ', datasDisponiveis)
+
+print('=' * 20)
+
+sala1 = int(input('Quantos participantes irão para a Sala 1? '))
+sala2 = int(input('E quantos serão os participantes da sala 2? '))
+sala3 = int(input('Por fim, os participantes na sala 3: '))
+qtdePessoas = sala1 + sala2 + sala3
+
+vanSala1 = math.ceil(sala1 / 12) # conforme solicitado no exercício
+vanSala2 = math.ceil(sala2 / 12)
+vanSala3 = math.ceil(sala3 / 12)
+qtdeVans = vanSala1 + vanSala2 + vanSala3
+
+orcamento = [vanSala1 * 350, vanSala2 * 350, vanSala3 * 350]
+orcamento.sort() # para ordenar os custos conforme solicitado no exercício
+print(f'Orçamentos ordenados: {orcamento}')
+
+valorTotal = sum(orcamento)
+
+print('=' * 20)
+print(f'RESUMO DO AGENDAMENTO PARA O DIA {dataConfirmada}:')
+print(f'- Total de pessoas: {qtdePessoas}')
+print(f'- Total de vans reservadas: {qtdeVans}')
+print(f'- Valor total para esse evento: R$ {valorTotal:.2f}')
+print('=' * 20)
